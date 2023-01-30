b53360d44c59db9a123ecff6eafe1aae3b6598dc
Pau Bertolin <paulaberos@gmail.com>
2023-01-30 15:44:39 +0100

    add 1 object to data file

--- data/tattooes.js
+++ data/tattooes.js
@@ -1,19 +1,8 @@
-//just for setup,, not working with our tattoo model
+//just for setup,, not working yet
 module.exports = [{
-    user: "Gener",
-    description: "Big area and space",
-    imageUrl: "https://i0.wp.com/anautrilla.com/wp-content/uploads/2017/06/decoracion-interiores-estilo-industrial-anautrilla-interiorismo-online.jpg?resize=1240%2C827&ssl=1",
-    owner: "63d13a00dc2dfc8c1c6ae363"
-  },
-  {
-    name: "Febrer",
-    description: "Big are and two levels",
-    imageUrl: "https://pavimentosmarcelo.com/wp-content/uploads/2022/01/Deco-Industrial.jpg",
-    owner: "63d139aa184d072f62b8dce9"
-  },
-  {
-    name: "Març",
-    description: "Big kitchen",
-    imageUrl: "http://3.bp.blogspot.com/-bq3GyslsYAo/UGLapqm_ysI/AAAAAAAAGAo/74TWLNTDNcU/s1600/marchi-loft_07.jpg",
-    owner: "63d1396213d1e7cdabe01712"
-}]
+    user: "63d7d69fcf6ec67040491acd",
+    tattooImage: "Big area and space",
+    year: 2005,
+    tattooer: "https://i0.wp.com/anautrilla.com/wp-content/uploads/2017/06/decoracion-interiores-estilo-industrial-anautrilla-interiorismo-online.jpg?resize=1240%2C827&ssl=1",
+    place: "Barcelona"
+  }]
