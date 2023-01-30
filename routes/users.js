4081b256b5987b76c1f89be2026d42542f555588
Estefania Egea <estefania.egcal@gmail.com>
2023-01-30 12:32:49 +0100

    profile get, profie edit get & post done

---
+++ routes/users.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// @desc Profile user
+// @route GET user/profile
+// @access Private
+router.get('/profile', /*middleware here */ function (req, res, next) {
+    const user = req.session.currentUser;
+    res.render('profile', user);
+});
+
+// @desc Profile user EDIT
+// @route GET user/profile/edit
+// @access Private
+router.get('/profile/edit', /*middleware here */ function (req, res, next) {
+    const user = req.session.currentUser;
+    res.render('profileEdit', user);
+});
+
+// @desc Profile user EDIT
+// @route POST user/profile/edit
+// @access Private
+router.post('/profile/edit', /*middleware here */ async function (req, res, next) {
+    const { username } = req.body;
+    const user = req.session.currentUser;
+    try {
+        const userInDB = await User.findByIdAndUpdate(user._id, { username }, { new: true });
+        req.session.currentUser = userInDB;
+        res.redirect('/profile');
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = router;
