4ef9354c066b14cf7719fe403143c403708adbf5
Pau Bertolin <paulaberos@gmail.com>
2023-01-30 11:47:14 +0100

    create User model

--- models/User.js
+++ models/User.js
@@ -1,24 +1,54 @@
 const { Schema, model } = require('mongoose');
  
 const userSchema = new Schema(
-  // Add whichever fields you need for your app
   {
     username: {
       type: String,
       trim: true,
-      required: [true, 'Username is required.'],
-      unique: true
+      required: [true, 'Username is required. Please add a username.'],
+      unique: true,
+      lowercase: true
     },
     email: {
       type: String,
-      required: [true, 'Email is required.'],
+      required: [true, 'Email is required.Please add an email.'],
       unique: true,
       lowercase: true,
       trim: true
     },
     hashedPassword: {
       type: String,
-      required: [true, 'Password is required.']
+      required: [true, 'Password is required. Please add a password.']
+    },
+    userRole: {
+      type: String,
+      enum: ['tattooer', 'user'],
+      default: 'Choose between user o tattooer role',
+      required: [true, 'Role is required. Please choose a user role.']
+    },
+    city: {
+      type: String,
+      required: [true, 'City is required. Please add what city are you living.']
+    },
+    tattooNumber: {
+      type: Number,
+      required: [true, 'Number of tattooes is required. Please add a number.']
+    },
+    profileImage: {
+      type: String,
+      default: 'https://www.idsplus.net/wp-content/uploads/default-placeholder.png'
+    },
+    profileDescription: {
+      type: String,
+    },
+    tattooStyle: {
+      enum: ['traditional-oldSchool', 'realism', 'watercolor', 'tribal', 'new school', 'neo traditional', 'japanese', 'blackwork', 'dotwork', 'geometric', 'illustrative', 'sketch', 'anime', 'lettering', 'minimalism', 'surrealism', 'trashPolka', 'blackAndGrey', 'ignorant']
+    },
+    studio: {
+      type: String
+    },
+    nextJourneys: {
+      type: String
     }
   },
   {
