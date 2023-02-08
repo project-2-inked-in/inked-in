785c183eb59305f06ba51cd3dd3c8abde9732fb7
Estefania Egea <estefania.egcal@gmail.com>
2023-02-08 14:36:22 +0100

    review no done

--- models/Review.js
+++ models/Review.js
@@ -3,14 +3,16 @@ const { Schema, model } = require('mongoose');
 const reviewSchema = new Schema(
     {
     username: {
-        type: String      
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true]
     },
     stars: {
         type: Number,
         required: true,
         min: 0,
         max: 5
-    },
+    }, //mirar si se puede hacer media para el perfil
     comment: {
         type: String
     }
